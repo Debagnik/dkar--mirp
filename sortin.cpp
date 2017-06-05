c89a43e90157b472ebcb35e9a4596f8eb141dd0d
Akashneel Saha <asaha-mirp@mit.edu>
2017-06-05 06:03:06 +0530

    day6

--- sortin.cpp
+++ sortin.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 int main()
 {
- int x[5]={100,2,3,4,5};
+ int x[5]={100,2,3,4,5},n;
  for(int i=0; i<5;i++)
  cout << x[i] <<" " ;
  char name[50]="my name";
  // print name using loops
  cout << "enter name\n";
  cin >> name;
- for(int i=0;i<50;i++)
+ n=strlen(name);
+ for(int i=0;i<n;i++)
  cout << name[i];
 
  // print name using loops
